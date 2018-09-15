dbac16f0be57a48dc1ad5bf75714d1f871d2d7bf
Aditya Seth <a.seth@foodpanda.com>
2018-09-15 11:28:58 +0200

    auth and post and cors

--- code/app.py
+++ code/app.py
@@ -1,17 +1,48 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from flask_cors import CORS
+
+from security import authenticate,identity
+
+
+axa_key = 'mountainous motion'
 
 app = Flask(__name__)
+app.secret_key = 'hellohack'
 api = Api(app)
+CORS(app)
+jwt = JWT(app, authenticate, identity)  # /auth
+
+patients = []
 
 
 class Patient(Resource):
-    def get(self, name):
-        return {'patient': name}
+    #@jwt_required
+    def get(self, id):
+        patient = next(filter(lambda x: x['id'] == id, patients), None)
+        return {'patient':patient}, 200 if patient else 404
 
-api.add_resource(Patient, '/patient/<string:name>')
+    def post(self, id):
+        if next(filter(lambda x: x['id'] == id, patients), None):
+            return {'message': 'A patient with id: {} Already exists'.format(id)}, 400
+        data = request.get_json()
+        print(data)
+        patient = {'id': id, 'name': data['name']}
+        patients.append(patient)
+        return data, 201
 
-app.run(port=5001)
+
+class PatientList(Resource):
+    def get(self):
+        return{'item':items}
+
+api.add_resource(PatientList, '/items')
+api.add_resource(Patient, '/patient/<int:id>')
+
+
+
+app.run(port=5001, debug=True)
 
 
 
