2c2199fc85f720de3b7255e82be7ffffd125a5dd
Johannes Kapfhammer <jkapfham@ethz.ch>
2018-09-15 09:39:08 +0200

    add menu

---
+++ patient-app/src/Emergency.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+// The Roster component matches one of two different routes
+// depending on the full pathname
+const Emergency = () => (
+    <p>Emergency</p>
+)
+
+
+export default Emergency
